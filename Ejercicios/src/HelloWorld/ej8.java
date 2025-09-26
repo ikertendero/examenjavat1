14d44655ad358ae462c17af8bde3a967caab1f4f
iker.tendero <iker.tendero@106-29.info2>
2025-09-26 12:41:28 +0200

    final

--- Ejercicios/src/HelloWorld/ej8.java
+++ Ejercicios/src/HelloWorld/ej8.java
@@ -1,5 +1,18 @@
 package HelloWorld;
-
+import java.util.Scanner;
 public class ej8 {
 
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Introduce tu nombre: ");
+	        String nombre = sc.nextLine();
+
+	        System.out.print("Introduce tu edad: ");
+	        int edad = sc.nextInt();
+
+	        System.out.println("Hola " + nombre + ", tienes " + edad + " años, ¡qué mayor eres!");
+
+	        sc.close();
+	 }
 }
